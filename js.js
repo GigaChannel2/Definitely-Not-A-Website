78e61aa2f83c6b0b457592ee98afa3b68661333a
Giga Channel <gigachannel115@gmail.com>
2025-03-08 05:44:41 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -66,7 +66,7 @@ function submit() {
             textCh("papua boi");
             break;
         case "naira putri":
-            goTo("https://www.google.com/search?client=ms-android-xiaomi-rvo2&sca_esv=b3124e538ea91f17&sxsrf=AHTn8zrUecQQOLvmKGHcYNZZs6AszjYSfQ:1741176592119&q=love+heart&uds=ABqPDvztZD_Nu18FR6tNPw2cK_RR_nOjWOfCgfo1dtdxBxE46H2ygKcZjw-VXJzRUActioIVjgE812u42QfrhOkjVPAHh3kp2RVvTf8mRbnR0NsJeyoOJPf-Rk_JZfqfcP72-x3sVK-1sDwIZLQWaJ4oBOxGtbIcDA&udm=2&sa=X&ved=2ahUKEwjUloyi9PKLAxUmxKACHa7ZAKwQxKsJegQICBAB&ictx=0&biw=501&bih=998&dpr=2.16");
+            goTo("https://www.google.com/search?q=cara+membuat+orang+jatuh+cinta&oq=cara+membuat+orang+jatuh+cinta&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIICAkQABgWGB4yCAgKEAAYFhgeMggICxAAGBYYHjIICAwQABgWGB4yCAgNEAAYFhgeMggIDhAAGBYYHtIBCTEzMzEzajBqNKgCDrACAfEFVzILYGVJ6x0&client=ms-android-xiaomi-rvo2&sourceid=chrome-mobile&ie=UTF-8");
             break;
         case "experiment 14":
             goTo("exp14.html");
