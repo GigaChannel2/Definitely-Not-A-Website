0d7bc15748c8092a69cffb1fce81e2e8c9cfec3e
Giga Channel <gigachannel115@gmail.com>
2026-04-09 03:00:42 +0700

    Add files via upload

--- pf.js
+++ pf.js
@@ -1,7 +1,48 @@
 let txt = document.getElementById("txt");
+let speed = 14300;
+let changed = 0;
 document.getElementById("container").style.opacity = 1;
 txt.textContent = "I loved you";
-setTimeout(() => {
-    txt.textContent = "She loved you and you killed her";
 
-}, 143000)
+text()
+setInterval(() => {
+    switch (changed) {
+        case 1:
+            speed = 14300;
+            break;
+        case 2:
+            speed = 1430;
+            break;
+        case 3:
+            speed = 143;
+            break;
+        case 43:
+            speed = 43;
+            break;
+        case 70:
+            speed = 14;
+            break;
+    }
+}, 14);
+
+function text() {
+    setTimeout(() => {
+        txt.textContent = "She loved you and you killed her";
+        setTimeout(() => {
+            txt.textContent = "He loved her and you killed her";
+            setTimeout(() => {
+                txt.textContent = "They loved her and you killed her";
+                if (changed < 143){                    
+                    changed += 1;
+                    text();
+                } else {
+                    txt.textContent = "You loved her and you killed her.";
+                }
+            }, speed)
+        }, speed)
+    }, speed)
+}
+// setTimeout(() => {
+//     txt.textContent = "She loved you and you killed her";
+//     text()
+// }, 143000)
